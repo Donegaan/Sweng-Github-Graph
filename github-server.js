bcb3bb9e3668fa1532f651d2e2cadc5534a72e13
Donegaan <donegaan@tcd.ie>
2017-11-23 21:42:35 +0000

    Initialised the server code needed for localhost using information from http://scottksmith.com/blog/2014/05/02/building-restful-apis-with-node/. I am also testing out committing from VSCode directly

---
+++ github-server.js
@@ -0,0 +1,26 @@
+//Initial code from http://scottksmith.com/blog/2014/05/02/building-restful-apis-with-node/
+
+// Get the packages we need, using express package, documentation: https://www.npmjs.com/package/express-package
+var express = require('express'); 
+
+// Create our Express application
+var app = express();
+
+// Use environment defined port or 3000
+var port = process.env.PORT || 3000;
+
+// Create our Express router
+var router = express.Router();
+
+// Initial dummy route for testing
+// http://localhost:3000/api
+router.get('/', function(req, res) {
+  res.json({ message: 'You are running dangerously low on beer!' });
+});
+
+// Register all our routes with /api
+app.use('/api', router);
+
+// Start the server
+app.listen(port);
+console.log('Insert beer on port ' + port);
