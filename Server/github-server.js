19607904df860c4bf4fad5ca0bc1e8a12ad6019e
Donegaan <donegaan@tcd.ie>
2017-11-26 18:06:58 +0000

    Created folders for views and server code, made a simple and clean html page.

--- Server/github-server.js
+++ Server/github-server.js
@@ -2,30 +2,32 @@
 
 // Get the packages we need, using express package, documentation: https://www.npmjs.com/package/express-package
 //alert("Test alert"); // To test if I have lnked the html file and the javascript file correctly
-var express = require('express'); 
+const express = require('express'); 
 var passport = require('passport'); // For the user authentication
-var fs = require('fs'); // File system for serving html page
+//var fs = require('fs'); // File system for serving html page
 
 // Create our Express application
-var app = express();
+const app = express();
 
+//View engine
+app.set('view engine','ejs');
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
 
 // Create our Express router
-var router = express.Router();
+//var router = express.Router();
 
 // Initial dummy route for testing
 // http://localhost:3000
-router.get('', function(req, res) {
-  res.writeHead(200, {'Content-Type' :'text/html'});
-  var readStream = fs.createReadStream(__dirname+'/github-info.html'); // Display html file on webpage
-  readStream.pipe(res);
+app.get('/', (req, res)=> {
+    res.render('homePage');
 });
 
+
 // Register all our routes with /api
-app.use('', router);
+//app.use('', router);
 
 // Start the server
-app.listen(port);
-console.log('Starting localhost on port ' + port);
+app.listen(port, ()=>{
+  console.log('Listening to port ' + port);
+});
