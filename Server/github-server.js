e483ac7060de3deca9c1952eefaac888e5a82454
Donegaan <donegaan@tcd.ie>
2017-11-27 12:39:53 +0000

    No displaying certain JSON data clearly instead of mass text.

--- Server/github-server.js
+++ Server/github-server.js
@@ -3,19 +3,11 @@
 // Get the packages we need, using express package, documentation: https://www.npmjs.com/package/express-package
 //alert("Test alert"); // To test if I have lnked the html file and the javascript file correctly
 const express = require('express'); 
-var passport = require('passport'); // For the user authentication
 var github = require('octonode'); // Github api library from: https://github.com/pksunkara/octonode
 //var fs = require('fs'); // File system for serving html page
 
 
-
-
-// {
-//   username: 'donegaan',
-//   password: 'password'
-// });
-
-var client = github.client('9238a976959a933e171c2d937fa017b85216d9e0'); // This is my access token, its email read only
+var client = github.client(); // This is my access token, its email read only
 
 // Create our Express application
 const app = express();
@@ -25,21 +17,27 @@ app.set('view engine','ejs');
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
 
-// Create our Express router
-//var router = express.Router();
+function jsonFormat(body){
+  //var str = JSON.stringify(body);
+  var formatStr = 'Username: ' + body.login
+  +'<br> ID: ' + body.id
+  +'<br> URL: ' + body.url
+  +'<br> Type: ' + body.type
+  +'<br> Name: ' + body.name
+  +'<br> Public Repos: ' + body.public_repos;
+  return formatStr;
+}
 
 // Initial dummy route for testing
-// http://localhost:3000
+// http://localhost:3000/
 app.get('/', (req, res)=> {
-  client.get('/user', {}, function (err, status, body, headers) {
-    res.send(JSON.stringify(body, null, 4));
+  client.get('/users/donegaan', {}, function (err, status, body, headers) {
+    var str = jsonFormat(body);
+    res.send(str);
+    console.log(body);
     //res.render('homePage');
   });
 });
-
-
-// Register all our routes with /api
-//app.use('', router);
 
 // Start the server
 app.listen(port, ()=>{
