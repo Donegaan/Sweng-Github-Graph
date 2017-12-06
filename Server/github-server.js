48e254e9fc02949a7c519857df4386799d2d0d7e
Donegaan <donegaan@tcd.ie>
2017-12-06 11:46:52 +0000

    Webpage now displays JSON data on org. However I think auth is needed to view the correct data. Also changed the graph.

--- Server/github-server.js
+++ Server/github-server.js
@@ -3,10 +3,15 @@
 // Get the packages we need, using express package, documentation: https://www.npmjs.com/package/express-package
 const express = require('express'); 
 var github = require('octonode'); // Github api library from: https://github.com/pksunkara/octonod
+var request = require('request');
 const flJson = require('./flare.json');
 
+var client = github.client();
+client.requestDefaults['proxy'] = 'https://myproxy.com:1085'// For the request library
 
-var client = github.client(); // This is my access token, its email read only
+
+var ghOrg = client.org('facebook');
+//ghOrg.info();
 
 // Create our Express application
 const app = express();
@@ -15,6 +20,12 @@ const app = express();
 app.set('view engine','ejs');
 // Use environment defined port or 3000
 var port = process.env.PORT || 3000;
+
+function callback(err, data, headers) {
+  console.log("error: " + err);
+  console.log("data: " + data);
+  console.log("headers:" + headers);
+}
 
 function jsonFormat(body){ // To display JSON data clearly
   //var str = JSON.stringify(body);
@@ -30,14 +41,15 @@ function jsonFormat(body){ // To display JSON data clearly
 // Initial dummy route for testing
 // http://localhost:3000/
 app.get('/', (req, res)=> {
-  client.get('/users/donegaan', {}, function (err, status, body, headers) {
+  //client.get('/users/donegaan', {}, function (err, status, body, headers) {
     // var str = jsonFormat(body);
-    // res.send(str);
+    //res.send(body);
+    res.send(ghOrg.members(callback));
     //console.log(body);
-    res.render('graph');
-  });
+    //res.render('graph');
+  //})
 });
-
+// Get the json file to populate the graph
 app.get('/flare.json', (req, res)=> {
     res.send(flJson);
 });
